75c56b919f1d659695b639f7158cb44a33f00aeb
pankhuri25 <pmn.bhatnagar@gmail.com>
2023-10-27 21:11:36 -0500

    add comments

--- consumer.py
+++ consumer.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import datetime
 import time
 import json
@@ -5,28 +6,35 @@ import logstash
 import yaml
 import random
 import logging
-
 from kafka import KafkaConsumer
 
+# Load configuration from 'app-config.yml'
 with open('app-config.yml') as f:
     props = yaml.safe_load(f)
 
+# Initialize the logger for the consumer
 def init_logger():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
+    
+    # Configure the Logstash logger handler
     logstash_host, logstash_port = props['logstash_server'].split(':')
     logger.addHandler(logstash.TCPLogstashHandler(logstash_host, logstash_port, version=1))
-    # Disable other loggers
+    
+    # Disable other loggers to focus on errors
     logging.getLogger().setLevel(logging.ERROR)
     return logger
 
 logger = init_logger()
-consumer_random_wait_time_params= props['consumer_random_wait_time_params']
+
+# Parameters for simulating random wait times and failure rates
+consumer_random_wait_time_params = props['consumer_random_wait_time_params']
 consumer_random_failure_rate_params = props['consumer_random_failure_rate_params']
 consumer_failure_rate = int(random.gauss(*consumer_random_failure_rate_params))
-failure_msgs_index = set(random.sample(range(100), consumer_failure_rate))
+failure_msgs_index = set(random.sample(range(100), consumer_failure_rate)
 message_count = 0
 
+# Create a function to generate log data for messages
 def get_log_data(message, status):
     return {
         'message_id': message['message_id'],
@@ -35,6 +43,7 @@ def get_log_data(message, status):
         'event_end_time': datetime.datetime.now().isoformat()
     }
 
+# Simulate sending a message to an actual customer
 def send_message_to_api_for_actual_delivery(message):
     global message_count
     message_count = (message_count + 1) % 100
@@ -43,24 +52,28 @@ def send_message_to_api_for_actual_delivery(message):
     time.sleep(random_wait_time)
 
     if message_count in failure_msgs_index:
+        # Log an error message for failed message delivery
         logger.error(f"Message delivery to end client failed", extra=get_log_data(message, 'FAILED'))
         return False
 
+    # Log a success message for messages successfully sent
     logger.info(f"Message successfully sent to mobile number", extra=get_log_data(message, 'SENT_TO_CLIENT'))
     return True
 
+# Message processing function
 def process_message(message):
     message = message.value.decode("utf-8")
     try:
         message = json.loads(message)
+        # Log a message when a message is successfully consumed
         logger.info("Message successfully received at consumer", extra=get_log_data(message, 'CONSUMED'))
         send_message_to_api_for_actual_delivery(message)
     except Exception as e:
+        # Log an error for failed message parsing
         logger.error(f"Message parsing failed for {message}")
 
-
+# Initialize the Kafka consumer and start consuming messages
 def init_consumer():
-    # Create a KafkaConsumer instance
     consumer = None
     while consumer is None:
         try:
@@ -71,10 +84,10 @@ def init_consumer():
             time.sleep(5)
         except:
             pass
-    # Start consuming messages
+
+    # Start consuming messages and processing them
     for message in consumer:
         process_message(message)
 
-
+# Start the Kafka consumer
 init_consumer()
-
--- producer.py
+++ producer.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import json
 import random
 import string
@@ -7,32 +8,38 @@ import logging
 import logstash
 import uuid
 import datetime
-
 from kafka.admin import KafkaAdminClient, NewTopic
 from kafka import KafkaProducer
 
+# Load configuration from 'app-config.yml'
 with open('app-config.yml') as f:
     props = yaml.safe_load(f)
 
+# Initialize the logger for the producer
 def init_logger():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
+    
+    # Configure the Logstash logger handler
     logstash_host, logstash_port = props['logstash_server'].split(':')
     logger.addHandler(logstash.TCPLogstashHandler(logstash_host, logstash_port, version=1))
-    # Disable other loggers
+    
+    # Disable other loggers to focus on errors
     logging.getLogger().setLevel(logging.ERROR)
     return logger
 
+# Initialize the Kafka producer and logger
 producer = None
 logger = init_logger()
 
-
+# Generate a random SMS message
 def generate_random_sms():
     phone_number = "".join(random.choices(string.digits, k=10))
-    message = "".join(random.choices(string.ascii_letters + string.digits, k=random.randint(1, 100)))
+    message = "".join(random.choices(string.ascii_letters + string.digits, k=random.randint(1, 100))
     message_id = str(uuid.uuid4())
     return {"phone_number": phone_number, "message": message, 'message_id': message_id}
 
+# Create log data for the produced message
 def get_log_data(message, status):
     return {
         'message_id': message['message_id'],
@@ -41,23 +48,27 @@ def get_log_data(message, status):
         'event_start_time': datetime.datetime.now().isoformat()
     }
 
+# Produce SMS messages to Kafka
 def produce_sms_messages():
     global producer
     num_messages = props['no_of_msgs_in_batch']  # Number of SMS messages to produce
+    
     if producer is None:
         producer = KafkaProducer(bootstrap_servers=props['bootstrap_servers'],
                                  value_serializer=lambda x: json.dumps(x).encode('utf-8'))
-
+    
     for _ in range(num_messages):
         sms_message = generate_random_sms()
         producer.send(props['kafka_topic'], value=sms_message)
         print(f"Message sent: {sms_message}")
+        
+        # Log a success message for the produced message
         logger.info("Message successfully sent", extra=get_log_data(sms_message, 'PRODUCED'))
-
-        # Delaying producer, to mimic real time producing effect, otherwise, buffer would overflow
+        
+        # Delay the producer to mimic real-time production
         time.sleep(10)
 
-
+# Create a Kafka topic
 def create_topic():
     try:
         admin_client = KafkaAdminClient(
@@ -76,7 +87,7 @@ def create_topic():
     except Exception as e:
         print("Exception while creating topic", e)
 
-
+# Main function to continuously produce messages
 def produce():
     while True:
         try:
@@ -85,5 +96,5 @@ def produce():
         except Exception as e:
             print(e)
 
-
+# Start the producer
 produce()
